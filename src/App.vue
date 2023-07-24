bd56f7466cb11b7269b8da381f479945e4ee9376
eric-kanno-dev <140447126+eric-kanno-dev@users.noreply.github.com>
2023-07-24 17:01:41 -0300

    first
    first

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app> <HomeViewVue></HomeViewVue> </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
+import HomeViewVue from "./views/HomeView.vue";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default Vue.extend({
+  name: "App",
+  components: {
+    HomeViewVue,
+  },
+  data: () => ({
+    //
+  }),
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
-</template>
+<template></template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Menu from "./../components/Menu.vue";
 
 export default Vue.extend({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
+
+  components: {},
 });
 </script>
